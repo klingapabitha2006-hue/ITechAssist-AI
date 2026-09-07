e0b1fce9c83401c27b2dbac5cb3febd77857d83e
Linga Pabitha K <klingapabitha2006@gmail.com>
2026-09-07 16:50:31 +0530

    Fix frontend CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ app = FastAPI(
  
 app.add_middleware( 
     CORSMiddleware, 
-    allow_origins=["*"], 
+    allow_origins=[
+    "https://itechassist-ai-frontend.onrender.com",
+    "http://127.0.0.1:5500"
+],
     allow_credentials=True, 
     allow_methods=["*"], 
     allow_headers=["*"], 
